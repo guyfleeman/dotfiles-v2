5282ce211f7f9fe115c326d8835aba8e1e773a68
Will Stuckey <guyfleeman@gmail.com>
2022-03-09 00:57:35 -0500

    fixed i3 multi DPI scaling

--- .dotfiles/window_manager/i3-gaps/graphical_sequence_startup.bash
+++ .dotfiles/window_manager/i3-gaps/graphical_sequence_startup.bash
@@ -5,20 +5,27 @@ if [ "$HOSTNAME" = "maven-dL01" ]; then
 	#xrandr --output DisplayPort-5 --left-of DisplayPort-4
 	#xrandr --output DisplayPort-4 --left-of DisplayPort-6
 	if xrandr | grep "HDMI-A-1 connected"; then
-		xrandr --output HDMI-A-1 --left-of DisplayPort-5 --scale 1x1 \
-			--output DisplayPort-5 --left-of DisplayPort-4 --scale 1x1 \
-			--output DisplayPort-4 --primary --scale 1x1 \
-			--output DisplayPort-6 --right-of DisplayPort-4 --scale 1x1
+		#xrandr --output HDMI-A-1 --left-of DisplayPort-5 --scale 1x1 \
+		#	--output DisplayPort-5 --left-of DisplayPort-4 --scale 1x1 \
+		#	--output DisplayPort-4 --primary --scale 1x1 \
+		#	--output DisplayPort-6 --right-of DisplayPort-4 --scale 1x1
+
+		xrandr --dpi 228 \
+			--output HDMI-A-1 --pos 0x0 --mode 3840x2160 --scale 1x1 \
+                	--output DisplayPort-5 --pos 3840x0 --mode 1920x1080 --scale 2x2 \
+                	--output DisplayPort-4 --pos 7680x0 --mode 2560x1440 --scale 1.5x1.5 --primary \
+                	--output DisplayPort-6 --pos 11520x0 --mode 1920x1080 --scale 2x2
 	else
-		xrandr --output DisplayPort-5 --left-of DisplayPort-4 --scale 1x1 \
-			--output DisplayPort-4 --primary --scale 1x1 \
-			--output DisplayPort-6 --right-of DisplayPort-4 --scale 1x1
+		xrandr --dpi 228 \
+                	--output DisplayPort-5 --pos 3840x0 --mode 1920x1080 --scale 2x2 \
+                	--output DisplayPort-4 --pos 7680x0 --mode 2560x1440 --scale 1.5x1.5 --primary \
+                	--output DisplayPort-6 --pos 11520x0 --mode 1920x1080 --scale 2x2
 	fi
 
 	compton -CG &
 
-	DISPLAY=:0 feh --bg-fill ~/.dotfiles/wallpapers/solarized_minimal.jpg
-	DISPLAY=:1 feh --bg-fill ~/.dotfiles/wallpapers/solarized_minimal.jpg
-	DISPLAY=:2 feh --bg-fill ~/.dotfiles/wallpapers/solarized_minimal.jpg
-	DISPLAY=:3 feh --bg-fill ~/.dotfiles/wallpapers/solarized_minimal.jpg
+	DISPLAY=:0 feh --bg-fill $1
+	DISPLAY=:1 feh --bg-fill $1
+	DISPLAY=:2 feh --bg-fill $1
+	DISPLAY=:3 feh --bg-fill $1
 fi
